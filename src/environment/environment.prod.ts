1fe022dba2db421f867bd2eef6ebae85a89cb20e
vo viet <90944284+hoangviet24@users.noreply.github.com>
2025-01-27 17:41:37 +0700

    Update .gitignore

--- src/environment/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    facebookAppId: '623730263610993'
-}
--- src/environment/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    facebookAppId: '623730263610993'
-  };
